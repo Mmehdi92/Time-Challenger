14d516d1ba91593a3b5506ec6e904360ee5589ca
Mehdi Zymberi <zymberi.mz@gmail.com>
2024-07-21 15:07:58 +0200

    added footer and change challenge name

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
       <Player />
       <div id="challenges">
         <TimerChallenge title={'Easy'}  targetTime={1}/>
-        <TimerChallenge title={'Easy'}  targetTime={5}/>
+        <TimerChallenge title={'Not Easy'}  targetTime={5}/>
         <TimerChallenge title={'Getting tough'}  targetTime={10}/>
         <TimerChallenge title={'Pros only'}  targetTime={15}/>
       </div>
